2a199fe56506d71540a266e073a5d7088a4d5a87
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-31 00:42:17 +0100

    ws service

--- frontend/src/app/core/services/ws.service.ts
+++ frontend/src/app/core/services/ws.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root'})
 export class WsService {
 
   constructor() { }
+
+  private ws?: WebSocket;
+  isConnected = signal(false);
+
+  connect(url: string, onMsg: (msg: any) => void){
+    this.ws = new WebSocket(url);
+    this.ws.binaryType = 'arraybuffer';
+    this.ws.onopen = () => this.isConnected.set(true);
+    this.ws.onclose = () => this.isConnected.set(false);
+    this.ws.onmessage = (ev) => {
+      try { onMsg(JSON.parse(ev.data));} catch (error) { onMsg(ev.data);}
+    };
+  }
+
+  sendJson(obj: any){
+    if ( this.ws?.readyState === WebSocket.OPEN) this.ws.send(JSON.stringify(obj));
+  }
+
+  sendBinary( buf: ArrayBuffer | Blob){
+    if ( this.ws?.readyState === WebSocket.OPEN) this.ws.send(buf);
+  }
 }
