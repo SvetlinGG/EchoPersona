7af71d1ba70678d8bef0f121bc4b326a0123e0f1
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-31 00:31:23 +0100

    audio service

--- frontend/src/app/core/services/audio.service.ts
+++ frontend/src/app/core/services/audio.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root'})
 export class AudioService {
-
   constructor() { }
+  private stream?: MediaStream;
+  private recorder?: MediaRecorder;
+  private chunkMs = 400;
+  isRecording = signal(false);
+
+  async start( onChunk: (blob: Blob) => void){
+    this.stream = await navigator.mediaDevices.getUserMedia({ audio: {echoCancellation: true, noiseSuppression: true}});
+    this.recorder = new MediaRecorder(this.stream, {mimeType: 'audio/webm;codecs=opus', audioBitsPerSecond: 48000 });
+    this.recorder.ondataavailable = (e) => e.data.size && onChunk(e.data);
+    this.recorder.start(this.chunkMs);
+    this.isRecording.set(true)
+  }
+
+  stop(){
+    this.recorder?.stop();
+    this.stream?.getTracks().forEach(t => t.stop());
+    this.isRecording.set(false);
+  }
 }
