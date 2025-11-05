50a2d57f5a21029ab59c8816f4724254ac3c874d
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-05 23:52:50 +0100

    setup settings component

--- frontend/src/app/features/settings/settings/settings.component.ts
+++ frontend/src/app/features/settings/settings/settings.component.ts
@@ -10,8 +10,23 @@ import { VoiceSettings } from '../../../core/services/models/settings';
 })
 export class SettingsComponent {
   voice = signal<VoiceSettings>({ voiceId: 'eleven_monolingual_v1', rate: 1, pitch: 0, volume: 1});
+  
+  updateVoiceId(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.voice.set({...this.voice(), voiceId: target.value});
+  }
+  
+  updateRate(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.voice.set({...this.voice(), rate: parseFloat(target.value)});
+  }
+  
+  updatePitch(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.voice.set({...this.voice(), pitch: parseInt(target.value)});
+  }
+  
   update<K extends keyof VoiceSettings>(k: K, v: VoiceSettings[K]){
     this.voice.set({...this.voice(), [k]: v});
   }
-
 }
