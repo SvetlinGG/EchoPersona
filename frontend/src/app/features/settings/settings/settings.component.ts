3d6d4cd20065608526def9b231abca56e983a490
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-05 23:41:09 +0100

    settings components

--- frontend/src/app/features/settings/settings/settings.component.ts
+++ frontend/src/app/features/settings/settings/settings.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { VoiceSettings } from '../../../core/services/models/settings';
 
 @Component({
   selector: 'app-settings',
+  standalone: true,
   imports: [],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css'
 })
 export class SettingsComponent {
+  voice = signal<VoiceSettings>({ voiceId: 'eleven_monolingual_v1', rate: 1, pitch: 0, volume: 1});
+  update<K extends keyof VoiceSettings>(k: K, v: VoiceSettings[K]){
+    this.voice.set({...this.voice(), [k]: v});
+  }
 
 }
